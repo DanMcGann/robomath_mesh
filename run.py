62ec9536c9207bcf87767c2f72d5ab7b53876697
Dan McGann <dmcgann@andrew.cmu.edu>
2020-12-17 15:57:16 -0500

    Final Commit

--- run.py
+++ run.py
@@ -5,9 +5,11 @@ from mesh_algos.mesh_algo import load_cloud, load_cloud_from_xyz
 import matplotlib.pyplot as plt
 
 
-file = "/home/daniel/Development/1-year/robomath_16881/robomath_mesh/data/terrain/a100_dome_007.xyz"
-cloud = load_cloud_from_xyz(file)
-bp = BallPivot(0.3)
+file = (
+    "/home/daniel/Development/1-year/robomath_16881/robomath_mesh/data/objects/cat.pcd"
+)
+cloud = load_cloud(file)
+bp = BallPivot(10)
 mesh = bp.generate_mesh(cloud)
 
 mesh.save_mesh("mesh.ply")
